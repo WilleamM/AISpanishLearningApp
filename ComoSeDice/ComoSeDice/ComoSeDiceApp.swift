f3b23265b2d7b9b11c85acf394e646cbbe958c07
WilleamM <mendezwilleam@icloud.com>
2026-02-17 20:05:29 -0800

    Added SwiftUI boilderplate code

---
+++ ComoSeDice/ComoSeDice/ComoSeDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComoSeDiceApp.swift
+//  ComoSeDice
+//
+//  Created by Willeam Mendez  on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct ComoSeDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComoSeDice/ComoSeDice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ComoSeDice
+//
+//  Created by Willeam Mendez  on 2/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
